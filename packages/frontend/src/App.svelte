89304078066650c76ed0a97d67e656a506fd82eb
faizisyellow <114222238+faizisyellow@users.noreply.github.com>
2025-12-07 20:28:39 +0700

    feat: add home page

--- packages/frontend/src/App.svelte
+++ packages/frontend/src/App.svelte
@@ -1,24 +1,9 @@
 <script>
+    import { Router, Route } from "svelte5-router";
+    import Home from "./pages/Home.svelte";
     import "./app.css";
-    import Counter from "./lib/Counter.svelte";
 </script>
 
-<main>
-    <h1>Vite + Svelte</h1>
-
-    <div class="card">
-        <Counter />
-    </div>
-
-    <p class="text-green-700 text-lg">Hello World !!</p>
-
-    <p class="read-the-docs">
-        Click on the Vite and Svelte logos to learn more
-    </p>
-</main>
-
-<style>
-    .read-the-docs {
-        color: #888;
-    }
-</style>
+<Router url="">
+    <Route path="/" component={Home} />
+</Router>
