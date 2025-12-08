c4c2e9dd4188ac44913e7a4461491ce6892aa8d6
faizisyellow <114222238+faizisyellow@users.noreply.github.com>
2025-12-08 10:34:43 +0700

    feat: compose homepage from sidebar, editor, and navbar component

--- packages/frontend/src/pages/Home.svelte
+++ packages/frontend/src/pages/Home.svelte
@@ -1,1 +1,58 @@
-<div>Home page</div>
+<script>
+    import Sidebar from "../components/Sidebar.svelte";
+    import Editor from "../components/Editor.svelte";
+    import Navbar from "../components/Navbar.svelte";
+
+    // percentage
+    const MAX_SIZE = 20;
+    const MIN_SIZE = 10;
+
+    class SidebarWidth {
+        #width = $state(0); // percentage
+        isResizing = $state(false);
+
+        constructor(width) {
+            this.#width = width;
+        }
+
+        get width() {
+            return this.#width;
+        }
+
+        set width(value) {
+            if (typeof value !== "number" || Number.isNaN(value)) {
+                return;
+            }
+
+            // clamp
+            this.#width = Math.max(MIN_SIZE, Math.min(value, MAX_SIZE));
+        }
+
+        startResize = (e) => {
+            this.isResizing = true;
+        };
+
+        onMove = (e) => {
+            if (!this.isResizing) return;
+
+            const percent = (e.clientX / window.innerWidth) * 100;
+
+            // update with setter
+            this.width = percent;
+        };
+
+        stopResize = () => {
+            this.isResizing = false;
+        };
+    }
+
+    const sidebarWidth = new SidebarWidth(MAX_SIZE);
+</script>
+
+<main>
+    <Navbar />
+    <div class="flex h-screen gap-x-2">
+        <Sidebar {sidebarWidth} />
+        <Editor remainSize={sidebarWidth.width} />
+    </div>
+</main>
