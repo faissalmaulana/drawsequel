dc372a1f9a8a66df3f5959f5188fe0a8b1ebe1c4
faizisyellow <114222238+faizisyellow@users.noreply.github.com>
2025-12-08 10:33:44 +0700

    feat: add sidebar component

---
+++ packages/frontend/src/components/Sidebar.svelte
@@ -0,0 +1,17 @@
+<script>
+    const { sidebarWidth } = $props();
+</script>
+
+<svelte:window
+    onpointermove={sidebarWidth.onMove}
+    onpointerup={sidebarWidth.stopResize}
+/>
+
+<div class=" bg-green-500 relative" style:width="{sidebarWidth.width}%">
+    <div
+        class="absolute top-0 right-0 w-1 h-full cursor-col-resize transition-colors"
+        role="separator"
+        aria-label="Resize sidebar"
+        onpointerdown={sidebarWidth.startResize}
+    ></div>
+</div>
