2d06dced7e0ca30500986acf6cbd8614d07108e6
faizisyellow <114222238+faizisyellow@users.noreply.github.com>
2025-12-09 17:07:33 +0700

    fix: add margin top in main area

--- packages/frontend/src/pages/Home.svelte
+++ packages/frontend/src/pages/Home.svelte
@@ -51,7 +51,7 @@
 
 <main>
     <Navbar />
-    <div class="flex h-screen gap-x-2">
+    <div class="flex h-screen gap-x-2 mt-3">
         <Sidebar {sidebarWidth} />
         <Editor occupiedWidth={sidebarWidth.width} />
     </div>
