7986bb9e10b2c655e1faa0e22b81a5ae40c30497
faizisyellow <114222238+faizisyellow@users.noreply.github.com>
2025-12-12 10:46:23 +0700

    feat: add table node component

--- packages/frontend/src/components/Editor.svelte
+++ packages/frontend/src/components/Editor.svelte
@@ -1,12 +1,15 @@
 <script>
     import { Background, Controls, SvelteFlow } from "@xyflow/svelte";
     import "@xyflow/svelte/dist/style.css";
+    import Node from "./Node.svelte";
 
     let { nodes = $bindable(), edges = $bindable() } = $props();
+
+    const nodeTypes = { tableNode: Node };
 </script>
 
 <div class="w-5/6 bg-blue-500">
-    <SvelteFlow bind:nodes bind:edges fitView>
+    <SvelteFlow bind:nodes bind:edges {nodeTypes} fitView>
         <Controls />
         <Background />
     </SvelteFlow>
---
+++ packages/frontend/src/components/Node.svelte
@@ -0,0 +1,27 @@
+<script>
+    let { data } = $props();
+</script>
+
+<div class="shadow bg-white border border-gray-200">
+    <div class="px-2 py-1 bg-green-700">
+        <p class="text-white text-sm font-semibold">{data.title}</p>
+    </div>
+
+    {#each data.list as menu}
+        <div
+            class="grid grid-cols-[28px_1fr_auto] items-center px-2 py-1 text-xs"
+        >
+            <p class="text-gray-400">
+                {menu.constraint}
+            </p>
+
+            <p>
+                {menu.name}
+            </p>
+
+            <p class="text-gray-400 whitespace-nowrap pl-4">
+                {menu.type}
+            </p>
+        </div>
+    {/each}
+</div>
