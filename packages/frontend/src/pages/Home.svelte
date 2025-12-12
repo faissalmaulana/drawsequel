43af45f0bf144c5db0c61d6c6bd9ff5042d43f46
faizisyellow <114222238+faizisyellow@users.noreply.github.com>
2025-12-12 10:46:47 +0700

    update: define dammy data for table nodes

--- packages/frontend/src/pages/Home.svelte
+++ packages/frontend/src/pages/Home.svelte
@@ -2,15 +2,80 @@
     import Sidebar from "../components/Sidebar.svelte";
     import Editor from "../components/Editor.svelte";
     import Navbar from "../components/Navbar.svelte";
+    import { SvelteFlowProvider } from "@xyflow/svelte";
 
-    let nodes = $state.raw([]);
-    let edges = $state.raw([]);
+    let nodes = $state.raw([
+        {
+            id: "1",
+            type: "tableNode",
+            position: { x: 100, y: 100 },
+            data: {
+                title: "Table Name ",
+                list: [
+                    {
+                        name: "column",
+                        type: "int",
+                        constraint: "I",
+                    },
+                    {
+                        name: "column",
+                        type: "string",
+                        constraint: "",
+                    },
+                    {
+                        name: "column",
+                        type: "string",
+                        constraint: "",
+                    },
+                    {
+                        name: "column",
+                        type: "string",
+                        constraint: "",
+                    },
+                ],
+            },
+        },
+        {
+            id: "2",
+            type: "tableNode",
+            position: { x: 400, y: 100 },
+            data: {
+                title: "Table Name ",
+                list: [
+                    {
+                        name: "column",
+                        type: "int",
+                        constraint: "I",
+                    },
+                    {
+                        name: "column",
+                        type: "string",
+                        constraint: "",
+                    },
+                    {
+                        name: "column",
+                        type: "string",
+                        constraint: "",
+                    },
+                    {
+                        name: "column",
+                        type: "string",
+                        constraint: "",
+                    },
+                ],
+            },
+        },
+    ]);
+
+    let edges = $state.raw([{ id: "e1-2", source: "1", target: "2" }]);
 </script>
 
 <main>
     <Navbar />
     <div class="flex h-screen gap-x-2 mt-3">
-        <Sidebar />
-        <Editor bind:nodes bind:edges />
+        <SvelteFlowProvider>
+            <Sidebar />
+            <Editor bind:nodes bind:edges />
+        </SvelteFlowProvider>
     </div>
 </main>
