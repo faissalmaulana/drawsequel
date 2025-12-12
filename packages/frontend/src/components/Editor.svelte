670caf0dab4b9acb1dcad3c8101b1db4638eeca7
faizisyellow <114222238+faizisyellow@users.noreply.github.com>
2025-12-12 09:06:20 +0700

    fix: width editor component

--- packages/frontend/src/components/Editor.svelte
+++ packages/frontend/src/components/Editor.svelte
@@ -2,10 +2,10 @@
     import { Background, Controls, SvelteFlow } from "@xyflow/svelte";
     import "@xyflow/svelte/dist/style.css";
 
-    let { nodes = $bindable(), edges = $bindable(), occupiedWidth } = $props();
+    let { nodes = $bindable(), edges = $bindable() } = $props();
 </script>
 
-<div class=" bg-blue-500" style:width="{100 - occupiedWidth}%">
+<div class="w-5/6 bg-blue-500">
     <SvelteFlow bind:nodes bind:edges fitView>
         <Controls />
         <Background />
