6ca746027d345aeafbe5c52364ac1ada773d160a
faizisyellow <114222238+faizisyellow@users.noreply.github.com>
2025-12-12 10:54:08 +0700

    update: remove resizer sidebar element

--- packages/frontend/src/components/Sidebar.svelte
+++ packages/frontend/src/components/Sidebar.svelte
@@ -1,10 +1,4 @@
 <script>
 </script>
 
-<div class="w-1/4 bg-green-500 relative">
-    <div
-        class="absolute top-0 right-0 w-1 h-full cursor-col-resize transition-colors"
-        role="separator"
-        aria-label="Resize sidebar"
-    ></div>
-</div>
+<div class="w-1/4 bg-green-500 relative"></div>
